25e6c0f8f48fa6b6e7d7c900efec86b706e507f2
kakp0 <143247183+kakp0@users.noreply.github.com>
2025-08-11 15:49:15 +0000

    Deploying to gh-pages from @ kakp0/CYBR372-Mid-Tri-Exam@457db9b9e7cb63aecba044a2b95de12c5ae41807 🚀

--- script.js
+++ script.js
@@ -443,7 +443,7 @@ displayLeaderboard(leaderboardData) {
                 <div class="leaderboard-score">
                     <span style="color: ${rankColor}; text-shadow: ${glowEffect};">${player.rank}</span>
                     <span class="leaderboard-accuracy">${player.accuracy.toFixed(1)}%</span>
-                    <span class="leaderboard-total-questions">${player.totalQuestions} Qs</span>
+                    <span class="leaderboard-total-questions">${player.totalQuestions} Total Questions</span>
                 </div>
             </div>
         `;
